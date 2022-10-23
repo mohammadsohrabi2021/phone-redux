3df75dfac43e0cc7c0deeb6418b1d41ebaa0742a
mohammad sohrabi <mohammadsohrabi141@gmail.com>
2022-10-23 11:17:13 +0330

    create home header

--- src/Componenets/Home/HomeHeader.jsx
+++ src/Componenets/Home/HomeHeader.jsx
@@ -1,9 +1,17 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
-function HomeHeader(props) {
+function HomeHeader() {
     return (
         <div>
-            
+            <h5>Mohammad Sohrabi</h5>
+            <div>
+                <Link to={'/addContact'}>
+                     <button>
+                        Add Contact
+                     </button>
+                </Link>
+            </div>
         </div>
     );
 }
