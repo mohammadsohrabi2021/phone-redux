e68d1bf6c55b36b4d60293f0aef7e024dd50696f
mohammad sohrabi <mohammadsohrabi141@gmail.com>
2022-10-23 11:37:25 +0330

    create singlePage

---
+++ src/Componenets/SinglePage/SinglePage.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { useParams } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { useEffect, useState } from "react";
+import SinglePageData from '../../Data/SinglePageData/SinglePageData';
+function SinglePage() {
+    const contacts = useSelector(state => state.contacts)
+    const { ContactId } = useParams()
+    const [contact, setContact] = useState({})
+
+    useEffect(() => {
+        setContact(contacts.filter(item => item.id === Number(ContactId))[0])
+    }, [])
+
+    return (
+        <div>
+            {SinglePageData.map(show => (
+                <div key={show.id}>
+                    {show.name} : {contact[show.name]}
+                </div>
+            ))}
+        </div>
+    );
+}
+
+export default SinglePage;
